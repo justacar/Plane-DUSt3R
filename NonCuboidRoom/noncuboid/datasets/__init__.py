9f96ccedaf6bf9674cf7aceebf8426e08353a25f
justacar <xyhuang0820@outlook.com>
2025-02-26 16:36:37 +0800

    delete

--- NonCuboidRoom/noncuboid/datasets/__init__.py
+++
@@ -1,5 +0,0 @@
-from noncuboid.datasets.custom import CustomDataset
-from noncuboid.datasets.nyu303 import NYU303
-from noncuboid.datasets.structured3d import Structured3D
-from noncuboid.datasets.sunrgbd import SUNRGBD
-from noncuboid.datasets.dust3r import Dust3rDataset
